574522a81e52ede35f16a6acb644b8889886fdde
Valentin LEPILLER <vlepille@student.42.fr>
2024-02-17 13:12:52 +0100

    Add some test for ex01

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -207,6 +207,15 @@ int	fail_tests(void)
 	catch(const std::exception& e)
 	{}
 
+	try
+	{
+		RPN::rpn("9 30 +");
+		std::cerr << __FILE__ << ": " << __LINE__ << ": " << "RPN::rpn(\"2 0 /\") should throw an exception but it doesn't" << std::endl;
+		ret += 1;
+	}
+	catch(const std::exception& e)
+	{}
+
 	return (ret);
 }
 
